c873bdcc360b3d87e85aeb712ad8771b6f46405f
chitresh99 <chitreshpoojary990@gmail.com>
2025-09-07 14:24:19 +0530

    refactor: drop market research agent from codebase

--- src/market research agent/basic_search.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.tools import DuckDuckGoSearchResults
-
-tool = DuckDuckGoSearchResults()
-
-query='What are the top 10 oil companies by valuation 2025'
-
-reponse = tool.invoke({"args": {"query":query}, "id": "1", "name": tool.name, "type": "tool_call"})
-
-print(reponse)
